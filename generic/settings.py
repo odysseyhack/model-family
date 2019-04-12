b251aa156900c5e69cd63e43127b766003c8feab
Olivier <o_louwaars@hotmail.com>
2019-04-12 19:01:37 +0200

    allowed host

--- generic/settings.py
+++ generic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '98fim!5fs6&coov+o1lk$!p9=x^92*0r*#rxgu7n=u5yr3(77j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.164.138']
+ALLOWED_HOSTS = ['172.16.164.138', '172.16.164.139']
 
 
 # Application definition
