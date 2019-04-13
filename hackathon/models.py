ed2f10bf134746e48524eeae3bc5fe9d87b2deca
mauriceFAM <mgr@fundament.nl>
2019-04-13 22:42:13 +0200

    models commit

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Building(models.Model):
+    bag_number = models.CharField(max_length=16)
+    building_year = models.IntegerField()
+    developer = models.CharField(max_length=256)
+    total_score = models.DecimalField(max_digits=10, decimal_places=2)
+
+class PropertyGroup(models.Model):
+    property_name = models.CharField(max_length=60)
+
+class BuildingProperty(models.Model):
+    title = models.CharField(max_length=60)
+    score_weight = models.DecimalField(max_digits=10, decimal_places=2)
+    building = models.ManyToManyField(Building)
+    expiration_date = models.DateTimeField()
+    property_group = models.ForeignKey(PropertyGroup, on_delete=models.CASCADE)
+
+class BuildingOwner(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    bsn_number = models.IntegerField()
+    buildings = models.ManyToManyField(Building)
+
+class BuildingMutator(models.Model):
+    company_name = models.CharField(max_length=60)
+    property_groups = models.ManyToManyField(PropertyGroup)
+
+
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# als je kennis hebt van squid dan kun je de hele ocean stack gebruiken
+#
+# github/oceanprocttocal/secret-store-pi
+#
+# tuna is testsuite van ocean, hiermee kun je testen
+#
+# ---------------
+# Data creator bepaald welke mensen met welke rollen data mogen inzien.
