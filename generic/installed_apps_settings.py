a8452cad17c1cc77312c80cc01811ae946382381
mauriceFAM <mgr@fundament.nl>
2019-04-14 11:40:42 +0200

    settings opgesplitst

---
+++ generic/installed_apps_settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    'rest_framework',
+    'hackathon',
+    'django_extensions',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'oauth2_provider',
+    'corsheaders'
+]
---
+++ generic/middleware_settings.py
@@ -0,0 +1,11 @@
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
