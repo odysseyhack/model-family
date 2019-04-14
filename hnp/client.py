af98a5790ced76dcc048319ad7bf598bdb31c160
mauriceFAM <mgr@fundament.nl>
2019-04-14 09:34:15 +0200

    client.py method refactoring

--- hnp/client.py
+++ hnp/client.py
@@ -13,17 +13,21 @@ def _update_keys(dictionary):
             dictionary.replace('-', '_')
         return dictionary
     else:
-        for k, v in dictionary.items():
-            if isinstance(v, dict):
-                v = _update_keys(v)
-            elif isinstance(v, list) and v is not None:
-                for vk, vv in enumerate(v):
-                    v[vk] = _update_keys(vv)
-            if k.find('-') > 0:
-                new[k.replace('-', '_')] = v
-            else:
-                new[k] = v
+        replace_dash_with_underscore(dictionary, new)
     return new
+
+
+def replace_dash_with_underscore(dictionary, new):
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            value = _update_keys(value)
+        elif isinstance(value, list) and value is not None:
+            for vk, vv in enumerate(value):
+                value[vk] = _update_keys(vv)
+        if key.find('-') > 0:
+            new[key.replace('-', '_')] = value
+        else:
+            new[key] = value
 
 
 class ApiRequestException(Exception):
@@ -42,16 +46,7 @@ class CreateApiRequest(object):
             request_url = "{0}{1}?oauth_token{2}".format(self.api_url, resource, self.token)
             req_hash = sha1(request_url.encode()).hexdigest()
             if cache.get(req_hash) is None:
-                req = requests.get("{0}/{1}".format(self.api_url, resource, args, kwargs),
-                                   params={'oauth_token': self.token, 'phase': 'SALE'})
-                json_response = ''
-                if req.status_code != 200:
-                    raise ApiRequestException(req.status_code)
-                else:
-                    if resource == '/about/version':
-                        json_response = json.dumps({"version": req.text})
-                    json_response = req.json()
-                cache.set(req_hash, json_response, 3600)
+                self.create_req_hash(args, kwargs, req_hash, resource)
             return cache.get(req_hash)
 
         except requests.exceptions.RequestException as e:
@@ -59,14 +54,24 @@ class CreateApiRequest(object):
             print(_("Connection error: %(exception)s" % {'exception': e}))
             sys.exit(1)
 
+    def create_req_hash(self, args, kwargs, req_hash, resource):
+        req = requests.get("{0}/{1}".format(self.api_url, resource, args, kwargs),
+                           params={'oauth_token': self.token, 'phase': 'SALE'})
+        json_response = ''
+        if req.status_code != 200:
+            raise ApiRequestException(req.status_code)
+        else:
+            if resource == '/about/version':
+                json_response = json.dumps({"version": req.text})
+            json_response = req.json()
+        cache.set(req_hash, json_response, 3600)
+
 
 class Client(object):
 
     def __init__(self, token, project_id=None):
         self.project_id = project_id
-
         self._api_request = CreateApiRequest(token=token)
-
         self.projects = []
         self.house_types = []
         self.houses = []
@@ -100,33 +105,32 @@ class Client(object):
         project = self._api_request.get(project_dict.get('link'))
 
         '''build the project involved parties (brokers)'''
+        self.build_brokers(project)
+
+        '''buid the project housetypes'''
+        project = self.build_project_house_types(project)
+        return project
+
+    def build_project_house_types(self, project):
+        housetypes_link = project.get('housetypes.link')
+        housetypes = self._api_request.get(housetypes_link)
+        project['housetypes'] = housetypes
+        del project['housetypes.link']
+        project = self.get_housetypes(project)
+        return project
+
+    def build_brokers(self, project):
         parties_link = project.get('brokers.link')
         parties = self._api_request.get(parties_link)
         project['involved_parties'] = parties
         del project['involvedparties.link']
 
-        '''buid the project housetypes'''
-        housetypes_link = project.get('housetypes.link')
-        housetypes = self._api_request.get(housetypes_link)
-        project['housetypes'] = housetypes
-        del project['housetypes.link']
-
+    def get_housetypes(self, project):
         if project.get('housetypes'):
             '''build the project houses'''
             _houses = []
             _projects = []
-            for link in [ht.get('self.link') for ht in project.get('housetypes')]:
-                house = self._api_request.get(link)
-                if house.get('houses'):
-                    house_c = house['houses']
-                    house['houses'] = list()
-                    for hc in house_c:
-                        if hc.get('status') != 'Verkocht':
-                            hc = _update_keys(hc)
-                            hc['housetype'] = house.get('name').lower().replace(' ', '-')
-                            # DONT FORGET HACK TODO
-                            house['houses'].append(hc)
-                    _projects.append(house)
+            self.get_houses(_projects, project)
 
             for project in _projects:
                 for houses in project.get('houses'):
@@ -134,8 +138,21 @@ class Client(object):
 
             if _houses:
                 project['houses'] = _houses
-
         return project
+
+    def get_houses(self, _projects, project):
+        for link in [ht.get('self.link') for ht in project.get('housetypes')]:
+            house = self._api_request.get(link)
+            if house.get('houses'):
+                house_c = house['houses']
+                house['houses'] = list()
+                for hc in house_c:
+                    if hc.get('status') != 'Verkocht':
+                        hc = _update_keys(hc)
+                        hc['housetype'] = house.get('name').lower().replace(' ', '-')
+                        # DONT FORGET HACK TODO
+                        house['houses'].append(hc)
+                _projects.append(house)
 
     def get_project(self, project_id=None):
         return self._build_details(project_id=project_id)
