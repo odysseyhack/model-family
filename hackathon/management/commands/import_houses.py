3792d9909e2e28a7456a6a81c2b9399476ec898f
mauriceFAM <mgr@fundament.nl>
2019-04-14 10:18:39 +0200

    command_handle refactor

--- hackathon/management/commands/import_houses.py
+++ hackathon/management/commands/import_houses.py
@@ -29,23 +29,18 @@ class Command(BaseCommand):
 					for column in data:
 						building_type = column[14]
 						self.process_building_type(appartment, building_type, column, farm, house, score_list)
-					print(sum(score_list)/len(score_list))
-					print(min(score_list))
-					print(max(score_list))
-					score_list.sort()
-					print(score_list[:5])
-					print(score_list[-5:])
+					self.print_and_sort(score_list)
+
 
 				except:
 					pass
 
-		def process_building_type(self, appartment, building_type, column, farm, house, score_list):
-			if building_type == house or building_type == appartment:
-				no_filled_cells = len([x for x in column if x])
-				no_total_cells = len(column) - 1
-				score = (no_filled_cells / no_total_cells) * 100
-				score_list.append(score)
-			elif building_type == farm:
-				logger.error('farm')
+		def print_and_sort(self, score_list):
+			print(sum(score_list) / len(score_list))
+			print(min(score_list))
+			print(max(score_list))
+			score_list.sort()
+			print(score_list[:5])
+			print(score_list[-5:])
 
 
